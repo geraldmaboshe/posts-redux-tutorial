1a6fa6c2e0dff70be4ff6c7c4dcc9fdb34282008
Gerald Maboshe <gerald@thezig.io>
2020-09-18 11:06:02 +0200

    displayed posts data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from './redux/effects/Posts';
+import { Post } from './redux/interfaces/Post';
+import { AppState } from './redux/store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function Posts() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+  const posts = useSelector((state: AppState) => state.posts);
+  const postItems = posts.posts.map((post: Post) => (
+    <div key={post.id}>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
-  );
+  ));
+  return <div>{postItems}</div>;
 }
-
-export default App;
